ae71b1e94483755460a46891160b2b9abd8f0caf
Geovani de Souza <geovani@serrabits.com.br>
2017-05-31 10:22:07 -0300

    Initial commit

---
+++ handler.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const next = require('next')
+const es = require('aws-serverless-express')
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const server = es.createServer(app.getRequestHandler())
+
+module.exports.hello = (event, context, callback) => {
+  // NOTE: aws-serverless-express uses context.succeed, but AWS already
+  // deprecated it in favor of callback
+  const fakeContext = {
+    succeed: res => callback(null, res)
+  }
+  app.prepare().then(() => es.proxy(server, event, fakeContext))
+}
