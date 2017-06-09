7080a45eae7eb2e172181f18704169d9d8449125
Geovani de Souza <geovani@serrabits.com.br>
2017-06-09 08:55:20 -0300

    API is now built from factory, using a repository pattern for persistence

--- aws/api.js
+++ aws/api.js
@@ -3,11 +3,19 @@
 const adapter = require('aws-serverless-express')
 const api = require('../lib/api')
 
-const server = adapter.createServer(api)
-
 module.exports.handler = (event, context, callback) => {
+  // TODO: Build a concrete repository
+  const repo = {}
+
+  // Create a server for a API instance
+  const server = adapter.createServer(api(repo))
+
+  // NOTE: aws-serverless-express uses context.succeed, but AWS already
+  // deprecated it in favor of callback
   const fakeContext = {
     succeed: res => callback(null, res)
   }
+
+  // Proxy the actual request
   adapter.proxy(server, event, fakeContext)
 }
