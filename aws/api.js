4bc69e29000799f6b1d0eb0a3e04d2c6ab1892a1
Geovani de Souza <geovani@serrabits.com.br>
2017-06-13 08:36:59 -0300

    Update api impl.

--- aws/api.js
+++ aws/api.js
@@ -4,15 +4,14 @@ const adapter = require('aws-serverless-express')
 const apiFactory = require('../lib/api')
 const repoFactory = require('./dynamodb_repo')
 
+// Build a concrete repository (DynamoDB as storage)
+const repo = repoFactory()
+
+// Create a server for a API instance
 const userIdGetter = req => req.requestContext.authorizer.claims.sub
+const server = adapter.createServer(apiFactory(repo, userIdGetter))
 
 module.exports.handler = (event, context, callback) => {
-  // Build a concrete repository (DynamoDB as storage)
-  const repo = repoFactory()
-
-  // Create a server for a API instance
-  const server = adapter.createServer(apiFactory(repo, userIdGetter))
-
   // NOTE: aws-serverless-express uses context.succeed, but AWS already
   // deprecated it in favor of callback
   const fakeContext = {
