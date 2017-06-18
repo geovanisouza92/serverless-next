9862b9c847d4e913f354024461b35c972aead676
Geovani de Souza <geovanisouza92@gmail.com>
2017-06-18 13:20:44 -0300

    Fix icon fonts loading

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
     require('postcss-easy-import')({ prefix: '_' }),
+    require('postcss-url')({
+      url: 'copy',
+      basePath: path.resolve(__dirname, 'node_modules/@blueprintjs/core/dist'),
+      assetsPath: 'static/resources'
+    }),
     require('autoprefixer')({ prefix: '_' })
   ]
 }
